745852fe055964814fa9b1544f4821840b744cf6
InitialDream <InitialMoon@sjtu.edu.cn>
2026-06-30 18:47:04 +0800

    fix: polyfill renderer randomUUID (#281)

--- src/app/renderer/main.tsx
+++ src/app/renderer/main.tsx
@@ -1,3 +1,5 @@
+import { ensureRandomUuid } from './bootstrap/ensureRandomUuid'
 import { renderApp } from './bootstrap/renderApp'
 
+ensureRandomUuid()
 renderApp()
--- src/app/renderer/web-main.tsx
+++ src/app/renderer/web-main.tsx
@@ -1,5 +1,7 @@
 import { installBrowserOpenCoveApi } from './browser/browserOpenCoveApi'
+import { ensureRandomUuid } from './bootstrap/ensureRandomUuid'
 import { renderApp } from './bootstrap/renderApp'
 
+ensureRandomUuid()
 installBrowserOpenCoveApi()
 renderApp()
